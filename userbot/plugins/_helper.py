0d0ffbb6daa347368ada62be52fe138d29998bb6
saskeuday <67370201+saskeuday@users.noreply.github.com>
2020-07-29 09:32:04 +0300

    Add files via upload

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -1,6 +1,8 @@
 from userbot import CMD_LIST
+from userbot.utils import admin_cmd
 
 @command(pattern="^.help ?(.*)")
+#@borg.on(admin_cmd(pattern=r"help ?(.*)"))
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
@@ -8,19 +10,28 @@ async def cmd_list(event):
         if tgbotusername is None or input_str == "text":
             string = ""
             for i in CMD_LIST:
-                string += "❤ " + i + "\n"
+                string += "🎭 " + i + "\n"
                 for iter_list in CMD_LIST[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
                 string += "\n"
             if len(string) > 4095:
-                await borg.send_message(event.chat_id, "Do .help cmd")
-                await asyncio.sleep(5)
+                with io.BytesIO(str.encode(string)) as out_file:
+                    out_file.name = "cmd.txt"
+                    await bot.send_file(
+                        event.chat_id,
+                        out_file,
+                        force_document=True,
+                        allow_cache=False,
+                        caption="**COMMANDS**",
+                        reply_to=reply_to_id
+                    )
+                    await event.delete()
             else:
                 await event.edit(string)
         elif input_str:
             if input_str in CMD_LIST:
-                string = "Commands found in {}:\n".format(input_str)
+                string = "Commands found in {}:".format(input_str)
                 for i in CMD_LIST[input_str]:
                     string += "    " + i
                     string += "\n"
